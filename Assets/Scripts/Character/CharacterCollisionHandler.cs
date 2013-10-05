87677f0521ca7d29584620a624cd3e09ec83240a
Ramon Rocha <ra.rocha.jr@gmail.com>
2013-10-05 14:42:02 -0500

    Revert 'Progress on collision jitters'
    We will be using a different approach.

--- Assets/Scripts/Character/CharacterCollisionHandler.cs
+++ Assets/Scripts/Character/CharacterCollisionHandler.cs
@@ -19,38 +19,25 @@ public class CharacterCollisionHandler : AbstractCollisionHandler {
     /// Stop the character's movement in the direction the collision came from.
     /// </summary>
     public override void HandleCollision(Collider collidedWith, Vector3 fromDirection, float distance, Vector3 normal) {
-        float hDistance = 0;
-        Vector3 hDirection = Vector3.Normalize(new Vector3(fromDirection.x, 0));
-        float vDistance = 0;
-        Vector3 vDirection = Vector3.Normalize(new Vector3(0, fromDirection.y));
 
         float theta = Vector3.Angle(fromDirection, normal);
 
         // If the wall or floor we've just run into is close enough to exactly opposite
         // to our velocity vector, then we'll just just stop our forward progress.
         if (theta <= 160f) {
+            float radians = theta * Mathf.Deg2Rad;
+
             // QUESTION: Does it make sense to pass the fromDirection.magnitude to the collision
             // handler methods since calculating the magnitude is an expensive operation?
-            float adjacentVectorMagnitude = fromDirection.magnitude * Mathf.Abs(Mathf.Cos(theta * Mathf.Deg2Rad));
+            float adjacentVectorMagnitude = fromDirection.magnitude * Mathf.Abs(Mathf.Cos(radians));
             Vector3 adjacentVector = normal * adjacentVectorMagnitude;
             Vector3 oppositeVector = fromDirection + adjacentVector;
 
             _character.velocity.x = oppositeVector.x;
             _character.velocity.y = oppositeVector.y;
-
-            float hAngle = Vector3.Angle(hDirection, fromDirection);
-            float vAngle = Vector3.Angle(vDirection, fromDirection);
-
-            hDistance = (distance - 0.01f) * Mathf.Cos(hAngle * Mathf.Deg2Rad) * hDirection.x;
-            vDistance = (distance - 0.01f) * Mathf.Cos(vAngle * Mathf.Deg2Rad) * vDirection.y;
         } else {
-            hDistance = (distance - 0.01f) * hDirection.x;
-            vDistance = (distance - 0.01f) * vDirection.y;
-
             _character.velocity.x = 0;
             _character.velocity.y = 0;
         }
-
-        _transform.position = new Vector3(_transform.position.x + hDistance, _transform.position.y + vDistance);
     }
 }

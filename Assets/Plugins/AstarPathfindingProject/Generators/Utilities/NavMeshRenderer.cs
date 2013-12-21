7d34066982d0e330323470ff70fed7a0b6204104
Eric Salczynski <eric@wehaventthetime.com>
2013-12-21 11:44:29 -0500

    Upgraded A* Pathfinding to 3.4 for Unity 2D physics

--- Assets/Plugins/AstarPathfindingProject/Generators/Utilities/NavMeshRenderer.cs
+++ Assets/Plugins/AstarPathfindingProject/Generators/Utilities/NavMeshRenderer.cs
@@ -5,27 +5,29 @@ using System.Collections;
 using UnityEditor;
 #endif
 
-[ExecuteInEditMode]
-public class NavMeshRenderer : MonoBehaviour {
-	
-	/** Last level loaded. Used to check for scene switches */
-	string lastLevel = "";
-	
-	/** Used to get rid of the compiler warning that #lastLevel is not used */
-	public string SomeFunction () {
-		return lastLevel;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		#if UNITY_EDITOR
-		if (lastLevel == "") {
-			lastLevel = EditorApplication.currentScene;
+namespace Pathfinding {
+	[ExecuteInEditMode]
+	public class NavMeshRenderer : MonoBehaviour {
+		
+		/** Last level loaded. Used to check for scene switches */
+		string lastLevel = "";
+		
+		/** Used to get rid of the compiler warning that #lastLevel is not used */
+		public string SomeFunction () {
+			return lastLevel;
 		}
 		
-		if (lastLevel != EditorApplication.currentScene) {
-			DestroyImmediate (gameObject);
+		// Update is called once per frame
+		void Update () {
+			#if UNITY_EDITOR
+			if (lastLevel == "") {
+				lastLevel = EditorApplication.currentScene;
+			}
+			
+			if (lastLevel != EditorApplication.currentScene) {
+				DestroyImmediate (gameObject);
+			}
+			#endif
 		}
-		#endif
 	}
 }
